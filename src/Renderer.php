0cdfc5cd78b4eab7c3fc92921fb89f22b3d2c748
Stephen Neal <stephen.neal14@gmail.com>
2020-12-08 14:25:21 -0500

    ADD src/Renderer & src/Renderer/AbstractRenderer from dompdf/dompdf 0.8.6

--- src/Renderer.php
+++ src/Renderer.php
@@ -29,14 +29,14 @@ class Renderer extends AbstractRenderer
      * @var AbstractRenderer[]
      */
     protected $_renderers;
-
+    
     /**
      * Cache of the callbacks array.
      *
      * @var array
      */
     private $_callbacks;
-
+    
     /**
      * Advance the canvas to the next page.
      */
@@ -44,7 +44,7 @@ class Renderer extends AbstractRenderer
     {
         $this->_canvas->new_page();
     }
-
+    
     /**
      * Render frames recursively.
      *
@@ -53,44 +53,44 @@ class Renderer extends AbstractRenderer
     public function render(Frame $frame)
     {
         global $_dompdf_debug;
-
+        
         $this->_check_callbacks('begin_frame', $frame);
-
+        
         if ($_dompdf_debug) {
             echo $frame;
             flush();
         }
-
+        
         $style = $frame->get_style();
-
+        
         if (in_array($style->visibility, ['hidden', 'collapse'])) {
             return;
         }
-
+        
         $display = $style->display;
-
+        
         // Starts the CSS transformation
         if ($style->transform && is_array($style->transform)) {
             $this->_canvas->save();
             list($x, $y) = $frame->get_padding_box();
             $origin = $style->transform_origin;
-
+            
             foreach ($style->transform as $transform) {
                 list($function, $values) = $transform;
                 if ($function === 'matrix') {
                     $function = 'transform';
                 }
-
+                
                 $values = array_map('floatval', $values);
                 $values[] = $x + (float) $style->length_in_pt($origin[0], (float) $style->length_in_pt($style->width));
                 $values[] = $y + (float) $style->length_in_pt($origin[1], (float) $style->length_in_pt($style->height));
-
+                
                 call_user_func_array([$this->_canvas, $function], $values);
             }
         }
-
+        
         switch ($display) {
-
+            
             case 'block':
             case 'list-item':
             case 'inline-block':
@@ -98,7 +98,7 @@ class Renderer extends AbstractRenderer
             case 'inline-table':
                 $this->_render_frame('block', $frame);
                 break;
-
+            
             case 'inline':
                 if ($frame->is_text_node()) {
                     $this->_render_frame('text', $frame);
@@ -106,28 +106,28 @@ class Renderer extends AbstractRenderer
                     $this->_render_frame('inline', $frame);
                 }
                 break;
-
+            
             case 'table-cell':
                 $this->_render_frame('table-cell', $frame);
                 break;
-
+            
             case 'table-row-group':
             case 'table-header-group':
             case 'table-footer-group':
                 $this->_render_frame('table-row-group', $frame);
                 break;
-
+            
             case '-dompdf-list-bullet':
                 $this->_render_frame('list-bullet', $frame);
                 break;
-
+            
             case '-dompdf-image':
                 $this->_render_frame('image', $frame);
                 break;
-
+            
             case 'none':
                 $node = $frame->get_node();
-
+                
                 if ($node->nodeName === 'script') {
                     if ($node->getAttribute('type') === 'text/php' ||
                         $node->getAttribute('language') === 'php'
@@ -141,32 +141,32 @@ class Renderer extends AbstractRenderer
                         $this->_render_frame('javascript', $frame);
                     }
                 }
-
+                
                 // Don't render children, so skip to next iter
                 return;
-
+            
             default:
                 break;
-
+            
         }
-
+        
         // Starts the overflow: hidden box
         if ($style->overflow === 'hidden') {
             list($x, $y, $w, $h) = $frame->get_padding_box();
-
+            
             // get border radii
             $style = $frame->get_style();
             list($tl, $tr, $br, $bl) = $style->get_computed_border_radius($w, $h);
-
+            
             if ($tl + $tr + $br + $bl > 0) {
                 $this->_canvas->clipping_roundrectangle($x, $y, (float) $w, (float) $h, $tl, $tr, $br, $bl);
             } else {
                 $this->_canvas->clipping_rectangle($x, $y, (float) $w, (float) $h);
             }
         }
-
+        
         $stack = [];
-
+        
         foreach ($frame->get_children() as $child) {
             // < 0 : nagative z-index
             // = 0 : no z-index, no stacking context
@@ -175,37 +175,37 @@ class Renderer extends AbstractRenderer
             $child_style = $child->get_style();
             $child_z_index = $child_style->z_index;
             $z_index = 0;
-
+            
             if ($child_z_index !== 'auto') {
                 $z_index = intval($child_z_index) + 1;
             } elseif ($child_style->float !== 'none' || $child->is_positionned()) {
                 $z_index = 1;
             }
-
+            
             $stack[$z_index][] = $child;
         }
-
+        
         ksort($stack);
-
+        
         foreach ($stack as $by_index) {
             foreach ($by_index as $child) {
                 $this->render($child);
             }
         }
-
+        
         // Ends the overflow: hidden box
         if ($style->overflow === 'hidden') {
             $this->_canvas->clipping_end();
         }
-
+        
         if ($style->transform && is_array($style->transform)) {
             $this->_canvas->restore();
         }
-
+        
         // Check for end frame callback
         $this->_check_callbacks('end_frame', $frame);
     }
-
+    
     /**
      * Check for callbacks that need to be performed when a given event
      * gets triggered on a frame.
@@ -218,7 +218,7 @@ class Renderer extends AbstractRenderer
         if (!isset($this->_callbacks)) {
             $this->_callbacks = $this->_dompdf->getCallbacks();
         }
-
+        
         if (is_array($this->_callbacks) && isset($this->_callbacks[$event])) {
             $info = [0 => $this->_canvas, 'canvas' => $this->_canvas,
                 1      => $frame, 'frame' => $frame, ];
@@ -234,7 +234,7 @@ class Renderer extends AbstractRenderer
             }
         }
     }
-
+    
     /**
      * Render a single frame.
      *
@@ -250,42 +250,42 @@ class Renderer extends AbstractRenderer
                 case 'block':
                     $this->_renderers[$type] = new Block($this->_dompdf);
                     break;
-
+                
                 case 'inline':
                     $this->_renderers[$type] = new Renderer\Inline($this->_dompdf);
                     break;
-
+                
                 case 'text':
                     $this->_renderers[$type] = new Text($this->_dompdf);
                     break;
-
+                
                 case 'image':
                     $this->_renderers[$type] = new Image($this->_dompdf);
                     break;
-
+                
                 case 'table-cell':
                     $this->_renderers[$type] = new TableCell($this->_dompdf);
                     break;
-
+                
                 case 'table-row-group':
                     $this->_renderers[$type] = new TableRowGroup($this->_dompdf);
                     break;
-
+                
                 case 'list-bullet':
                     $this->_renderers[$type] = new ListBullet($this->_dompdf);
                     break;
-
+                
                 case 'php':
                     $this->_renderers[$type] = new PhpEvaluator($this->_canvas);
                     break;
-
+                
                 case 'javascript':
                     $this->_renderers[$type] = new JavascriptEmbedder($this->_dompdf);
                     break;
-
+                
             }
         }
-
+        
         $this->_renderers[$type]->render($frame);
     }
 }
