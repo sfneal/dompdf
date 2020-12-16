8d2fc2138a0bdd817fc08eea9254e89e098d3964
Stephen Neal <stephen.neal14@gmail.com>
2020-12-16 12:55:52 -0500

    START fixing issue with summing of strings & float values

--- src/Renderer/Inline.php
+++ src/Renderer/Inline.php
@@ -86,8 +86,10 @@ class Inline extends AbstractRenderer
         }
 
         // Add the border widths
-        $w += (float) $widths[1] + (float) $widths[3];
-        $h += (float) $widths[0] + (float) $widths[2];
+        $w += floatval($widths[1]) + floatval($widths[3]);
+        $h += floatval($widths[0]) + floatval($widths[2]);
+//        $w += (float) $widths[1] + (float) $widths[3];
+//        $h += (float) $widths[0] + (float) $widths[2];
 
         // If this is the first row, draw the left border too
         if ($bp['left']['style'] !== 'none' && $bp['left']['color'] !== 'transparent' && $widths[3] > 0) {
