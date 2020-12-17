5c93886ccf1b106626d1bc31578ba87673c0c7ab
Stephen Neal <sfneal@users.noreply.github.com>
2020-12-17 16:41:21 +0000

    Apply fixes from StyleCI

--- src/Css/Style.php
+++ src/Css/Style.php
@@ -581,7 +581,7 @@ class Style
                 return $cache[$key];
             }
         }
-    
+
         \Illuminate\Support\Facades\Log::error(json_encode([
             '$length,'=> $length,
         ]));
